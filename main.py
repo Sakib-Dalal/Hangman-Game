ed0adffe67ca94f0578c541d6845be6edeb72688
Sakib_Dalal <sakibdalal73@gmail.com>
2023-12-30 11:28:25 +0530

    Step 4

--- main.py
+++ main.py
@@ -1,7 +1,69 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+
+lives = 6
 
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -9,18 +71,24 @@ display = []
 for _ in range(word_length):
     display += "_"
 
-end_of_game = False
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
-    print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
         end_of_game = True
-        print("You Win")
+        print("You win.")
+
+    print(stages[lives])
