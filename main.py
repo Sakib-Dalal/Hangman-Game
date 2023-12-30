8c21cb0ad3e57aa3ee4456665433df82557ea9e8
Sakib_Dalal <sakibdalal73@gmail.com>
2023-12-30 10:13:28 +0530

    Step 2

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-chose = random.choice(word_list)
+chosen_word = random.choice(word_list)
 
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+
+for i in chosen_word:
+    display.append("_")
+
+print(display)
 guess = input("Guess a letter: ").lower()
 
-for i in range(len(chose)):
-    if guess == chose[i]:
+for i in range(len(chosen_word)):
+    if chosen_word[i] == guess:
+        display[i] = chosen_word[i]
+    else:
+        display[i] = "_"
+
+for letter in chosen_word:
+    if letter == guess:
         print("Right")
     else:
         print("Wrong")
+
+print(display)
